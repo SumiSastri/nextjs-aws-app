1588115e8218bb25fc5562f355feaf70b9be6132
Sumi <sumi.sastri@cancer.org.uk>
2022-09-26 18:36:00 +0100

    Add graphql library and intialise methods server side

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nextjs-aws-app
 Full stack app with AWS Amplify-Cognito-GraphQL-MongoDB-Express
+
+# Scaffolding
+
+- `npm init` initialise with node
+- add `.gitignore` file and server side folde
+- `npm install --y` install node modules
+- `npm install express mongoose helmet cors colors nodemon graphql express-graphql` server-side dependencies
+
--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const port = process.env.PORT || 4000;
 
+const port = process.env.PORT || 4000;
 const helmet = require('helmet');
 const colors = require('colors');
 
+// intialise methods
 const app = express();
 app.use(cors());
 app.use(helmet({
   contentSecurityPolicy: false,
 }));
+// use the grapqlHTTP library methods
+app.use('/graphql', graphqlHTTP({
+  // schema,
+  // if environment changes to production this changes to false
+  graphiql: true
+}));
 
 
+// connect to db and server
 app.listen(4000, () => console.log(`NEXTJS-AWS-APP: Express server connected on port ${port}`));
