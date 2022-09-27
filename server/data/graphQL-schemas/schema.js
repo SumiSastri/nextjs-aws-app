e68a780808f52bb02c83cabcc897950c219a5dde
Sumi <sumi.sastri@cancer.org.uk>
2022-09-27 15:39:14 +0100

    Add hobbies types and root query

--- server/data/graphQL-schemas/schema.js
+++ server/data/graphQL-schemas/schema.js
@@ -2,50 +2,85 @@ const graphql = require("graphql");
 const _ = require("lodash");
 
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLID,
 } = graphql
 
 // hard code and test resolve in the graphQL-express server
- const userData = [
-  {id: "User1", phoneNumber: 123 , email: "email1@email.com", createdAt: Date.now() },
-  {id: "User2", phoneNumber: 456 , email: "email2@email.com", createdAt: Date.now() },
-  {id: "User3", phoneNumber: 789 , email: "email3@email.com", createdAt: Date.now() },
-  {id: "User4", phoneNumber: 013 , email: "email4@email.com", createdAt: Date.now() },
-  ]
+const usersData = [
+  { id: "User1", name: "Janaki", profession: "Butcher", phoneNumber: 123, email: "janaki@email.com", age: 11, createdAt: Date.now() },
+  { id: "User2", name: "Jyostna", profession: "Baker", phoneNumber: 456, email: "jyotsna@email.com", age: 22, createdAt: Date.now() },
+  { id: "User3", name: "Jananani", profession: "Candlestick Maker", phoneNumber: 789, email: "janani@email.com", age: 31, createdAt: Date.now() },
+  { id: "User4", name: "Jhanavi", profession: "Fish Monger", phoneNumber: 013, email: "jhanavi@email.com", age: 14, createdAt: Date.now() },
+]
 
-// Create types - this is the parent
+const hobbiesData = [
+  { id: "Hobby1", title: "Cooking", description: "Barbecues and Grills", createdAt: Date.now() },
+  { id: "Hobby2", title: "Baking", description: "Breads and sweets baking", createdAt: Date.now() },
+  { id: "Hobby3", title: "Decorating", description: "Home decorations", createdAt: Date.now() },
+  { id: "User4", title: "Fishing", description: "Fresh water fly fishing", createdAt: Date.now() },
+]
+
+// Create types
 const UserType = new GraphQLObjectType({
-    name: "User",
-    description: "Documentation for user...",
-    // data blueprint of the types and data requested
-    fields: () => ({
-        id: {type:GraphQLString},
-        email: {type:GraphQLString},
-        phoneNumber: {type:GraphQLInt}
-    }) 
+  name: "User",
+  description: "Documentation for user...",
+  // data blueprint of the types and data requested
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phoneNumber: { type: GraphQLInt },
+    profession: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+})
+
+const HobbyType = new GraphQLObjectType({
+  name: "Hobby",
+  description: "Hobbies and interests ...",
+  fields: () => ({
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    description: { type: GraphQLString },
+  })
 })
 
 // RootQuery - traverse the data
 const RootQuery = new GraphQLObjectType({
-    name: "RootQueryType",
-    description: "Description",
-    fields: {
-      user: {
-        type: UserType,
-        args: { id: { type: GraphQLString } },
-        resolve(parent, args) {    
-          return _.find(userData, 
-            {id: args.id}
-            )
-        },
+  name: "RootQueryType",
+  description: "Description",
+  fields: {
+    // Users
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // return users data   
+        return _.find(usersData,
+          { id: args.id }
+        )
       },
-
     },
-  });
-  
-  module.exports = new GraphQLSchema({
-    query: RootQuery,
-  });
+    // Hobbies
+    hobby: {
+      type: HobbyType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // return hobbies data 
+        return _.find(hobbiesData,
+          { id: args.id }
+        )
+      },
+    },
+
+
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
