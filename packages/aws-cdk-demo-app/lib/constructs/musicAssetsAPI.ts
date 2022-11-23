bf1d7b5780cd3d340e73c949d04ce17eb7f45769
Sumi <sumi.sastri@cancer.org.uk>
2022-11-23 12:39:50 +0000

    Clean up constructs - remove logs

--- packages/aws-cdk-demo-app/lib/constructs/musicAssetsAPI.ts
+++ packages/aws-cdk-demo-app/lib/constructs/musicAssetsAPI.ts
@@ -1,13 +1,10 @@
-import { MusicAssetsAPIProps } from './musicAssetsAPI';
 import { Construct } from 'constructs';
 import {Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
 // import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
 // import * as s3 from 'aws-cdk-lib/aws-s3';
-
-// import * as apigateway from '@aws-cdk/aws-apigateway';
-
 import * as path from 'path';
+
 
 // interface of I Bucket - props gives it a bucket name
 export interface MusicAssetsAPIProps  {
@@ -18,23 +15,14 @@ export class MusicAssetsAPI extends Construct {
     constructor (scope: Construct, id: string, props?: MusicAssetsAPIProps){
         super (scope, id);
 
-//  LAMBDA for S3 - S3 stores static assets like pdfs/ images
-// pass a reference of s3 into the lamdba construct
+//  LAMBDA for S3 
 const getMusicAssetsFunction = new Function(this, "GetMusicAssetsFunction", {
   runtime: Runtime.NODEJS_16_X,
-// CONSTRUCTS LOCATION - bundles code from file path - must be the directory 
   code: Code.fromAsset(path.join(__dirname, '../../', "api", 'get-music-assets')),
-// HANDLERS - bundle code for minifying & build process
   handler: "getMusicAssets",  
-  // deprecated as a Function Prop?
-  // bundling: {
-  //   externalModules: [
-  //   "aws-sdk"
-  //   ]
-  //   },
-  environment: {
-    MUSIC_ASSETS_BUCKET: props.musicAssetsBucket.bucketName
-},
+//   environment: {
+//     MUSIC_ASSETS_BUCKET: props.musicAssetsBucket.bucketName
+// },
 });
     }
 }
--- packages/aws-cdk-demo-app/lib/constructs/networking.ts
+++ packages/aws-cdk-demo-app/lib/constructs/networking.ts
@@ -16,21 +16,15 @@ export class Networking extends Construct {
         super(scope, id);
 
         new ec2.Vpc(this, 'AWSCDKDemoVPC', {
-            // cidr: '10.0.0.0/16', to be deprecated
             ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
             maxAzs: props.maxAzs,
-            // maxAzs:2 -> you can specify a number but using props more dynamic
+            // maxAzs:2 -> using props means you can set the number dynamically when used elsewhere in code
             subnetConfiguration: [
                {
                  cidrMask: 24,
                  name: 'AWSCDKDemoPublic',
                  subnetType: ec2.SubnetType.PUBLIC,
                },
-            //    {
-            //      cidrMask: 24,
-            //      name: 'AWSCDKDemoPrivate',
-            //      subnetType: ec2.SubnetType.PRIVATE_AWS_CDK_DEMO,
-            //    }
             ]
          });
 
