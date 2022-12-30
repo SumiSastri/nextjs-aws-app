19c6f9034469578b8002f42dd0c22fdd18a80aa6
Sumi <sumi.sastri@cancer.org.uk>
2022-12-30 16:14:35 +0000

    Clean up server files

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,34 +1,50 @@
-const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-const {mongoose} = require('mongoose');
-const {colors} = require('colors');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const { mongoose } = require("mongoose");
+const { colors } = require("colors");
+const cors = require("cors");
+const helmet = require("helmet");
 // config
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 4000;
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI;
+const hostname = "127.0.0.1";
 
-const schema = require('./data/graphQL-schemas/schema');
+const schema = require("./data/graphQL-schemas/schema");
 
 const app = express();
 app.use(cors());
-app.use(helmet({
-  contentSecurityPolicy: false,
-}));
-app.use('/graphql', graphqlHTTP({
-  schema,
-  // if environment changes to production this changes to false
-  graphiql: true
-}));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    // if environment changes to production this changes to false
+    graphiql: true,
+  })
+);
 
-mongoose.connect(`mongodb+srv://music-app-user:ibzmMOfoHN7Gug2K@cluster0.slip5.mongodb.net/test`, 
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }) .then(() => console.log('SUCCESS: MongoDB Connected...'.cyan.underline.bold))
-  .catch(err => console.log(err, "FAIL: Not connected Check cloud connection, password, IP address".red.underline.bold));
-  
-app.listen(process.env.PORT || 4000, '0.0.0.0', () => {
-  console.log(`NEXT-JS_AWS_APP: Express server connected`.green.underline.bold);
+// update IP address with a VPN
+mongoose
+  .set("strictQuery", false)
+  .connect(MONGO_URI)
+  .then(() => console.log("SUCCESS: MongoDB Connected...".cyan.underline.bold))
+  .catch((err) =>
+    console.log(
+      err,
+      "DEBUG-CHECK: cloud connection, password, IP address (OR) node --trace-deprecation"
+        .red.underline.bold
+    )
+  );
+// console.log(mongoose.connect());
+app.listen(process.env.PORT || 4000, "0.0.0.0", hostname, () => {
+  console.log(
+    `NEXT-JS_AWS_APP: Express server connected on port: ${port}`.green.underline
+      .bold
+  );
 });
+// console.log(app.listen());
